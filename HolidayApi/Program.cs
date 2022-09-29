c307908f009813d1d0434d80aca5bea28098dff0
Egoushka <egorgrabovskij@gmail.com>
2022-09-29 20:16:33 +0300

    Updated swagger

--- HolidayApi/Program.cs
+++ HolidayApi/Program.cs
@@ -1,7 +1,10 @@
+using System.Reflection;
 using HolidayApi.Interfaces;
 using HolidayApi.Profiles;
 using HolidayApi.Services;
 using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +13,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "A simple example ASP.NET Core Web API",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "My Name",
+            Email = string.Empty,
+            Url = new Uri("https://example.com/twitter"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Use under MIT",
+            Url = new Uri("https://example.com/license"),
+        }
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath, true);
+});
 builder.Services.AddHttpClient();
 builder.Services.AddAutoMapper(typeof(HolidayProfile));
 builder.Services.AddHttpClient("getSupportedCountries", httpClient =>
@@ -33,7 +59,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        c.DocumentTitle = "Todo APIs";
+        c.DocExpansion(DocExpansion.None);
+        c.RoutePrefix = string.Empty;
+    });
 }
 app.UseHttpsRedirection();
 
