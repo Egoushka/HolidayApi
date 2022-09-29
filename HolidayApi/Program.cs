f3cfde263dd955f4e0b679c2f7618ab80108fca2
Egoushka <egorgrabovskij@gmail.com>
2022-09-29 20:32:36 +0300

    Update swagger again

--- HolidayApi/Program.cs
+++ HolidayApi/Program.cs
@@ -13,30 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "ToDo API",
-        Description = "A simple example ASP.NET Core Web API",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
-        {
-            Name = "My Name",
-            Email = string.Empty,
-            Url = new Uri("https://example.com/twitter"),
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Use under MIT",
-            Url = new Uri("https://example.com/license"),
-        }
-    });
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath, true);
-});
+builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
 builder.Services.AddAutoMapper(typeof(HolidayProfile));
 builder.Services.AddHttpClient("getSupportedCountries", httpClient =>
@@ -59,13 +36,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        c.DocumentTitle = "Todo APIs";
-        c.DocExpansion(DocExpansion.None);
-        c.RoutePrefix = string.Empty;
-    });
+    app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
 
